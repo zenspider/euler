46bd455cdb9edfca22ee03fecdf4a74f79a46c42
Ryan Davis <ryand-ruby@zenspider.com>
2014-05-01 01:02:10 -0700

    activated 036

--- 036/main.rb
+++ 036/main.rb
@@ -1,4 +1,5 @@
-# Euler #666:
+require "maffs"
 
-# p (1..1_000_000).find_all { |n| n.palindrome? && n.to_s(2).palindrome? }.sum
+# Euler #036:
 
+p (1..1_000_000).find_all { |n| n.palindrome? && n.to_s(2).palindrome? }.sum
